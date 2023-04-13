88bda527a3872c2f8c21ddf7ce3dc3e80a31a87c
marcos <marcosep192000@hotmail.com>
2023-04-12 21:50:11 -0300

    add Product

--- src/main/java/com/agro/admin/models/entity/Product.java
+++ src/main/java/com/agro/admin/models/entity/Product.java
@@ -1,15 +1,9 @@
 package com.agro.admin.models.entity;
-
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
-
 @Data
 @Entity
 @Table(name = "product")
@@ -27,7 +21,4 @@ public class Product {
 	@UpdateTimestamp
 	@Column(name = "updated_on_date")
 	private LocalDateTime updateDate;
-
-
-
 }
