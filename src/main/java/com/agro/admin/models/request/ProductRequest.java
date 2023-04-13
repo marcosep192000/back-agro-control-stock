be462f9aa697ab075a8d8d56cde3698388f35027
marcos <marcosep192000@hotmail.com>
2023-04-12 21:14:38 -0300

    add ProductRequest

---
+++ src/main/java/com/agro/admin/models/request/ProductRequest.java
@@ -0,0 +1,28 @@
+package com.agro.admin.models.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+	@NotBlank(message = "name is not empty!")
+	private  String nameProduct;
+	private String descriptionProduct;
+	private float  priceDls ;
+	private  float iva;
+	private boolean state = true;
+	@UpdateTimestamp
+	@Column(name = "updated_on_date")
+	private LocalDateTime updateDate;
+
+
+
+}
