2719b0e2640645d3334fe3c269d3d50088910b9c
marcos <marcosep192000@hotmail.com>
2023-04-12 21:48:39 -0300

    add ProductServiceImpl atributes

--- src/main/java/com/agro/admin/service/IProductService.java
+++ src/main/java/com/agro/admin/service/IProductService.java
@@ -3,12 +3,13 @@ package com.agro.admin.service;
 import com.agro.admin.models.entity.Product;
 import com.agro.admin.models.request.ProductRequest;
 import com.agro.admin.models.response.ProductResponse;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface IProductService {
 
-	Product createProduct(ProductRequest request);
+	ResponseEntity<Product> createProduct(ProductRequest request);
 	Product updateProduct( Long id) ;
 	ProductResponse softDelete (Long id ,boolean state);
 	List<ProductResponse> allProduct();
