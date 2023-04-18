c9debc892ab02f9a41b5324b07653f64e55e0e30
marcos <marcosep192000@hotmail.com>
2023-04-18 08:43:37 -0300

    add configuration ProviderServiceImpl

--- src/main/java/com/agro/admin/service/IProviderService.java
+++ src/main/java/com/agro/admin/service/IProviderService.java
@@ -1,4 +1,19 @@
 package com.agro.admin.service;
 
+import com.agro.admin.models.entity.Provider;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IProviderService {
+
+
+    public List<Provider> getAllUsers() ;
+
+    public Optional<Provider> getUserById(Long id) ;
+    public Provider createUser( Provider Provider);
+
+    public Provider updateUser(Provider Provider);
+
+    public void deleteUser(Long id) ;
 }
