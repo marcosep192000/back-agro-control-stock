ad7aaf85888eccce495bca7025c7faece2896cbb
marcos <marcosep192000@hotmail.com>
2023-04-24 22:08:24 -0300

    modify ProviderResponse

--- src/main/java/com/agro/admin/models/response/ProviderResponse.java
+++ src/main/java/com/agro/admin/models/response/ProviderResponse.java
@@ -1,27 +1,24 @@
 package com.agro.admin.models.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-@Data
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProviderResponse {
 	private Long id;
 	@NotBlank(message = " i can't be empty ")
 	private String nameProvider;
-	@NotBlank(message = " i can't be empty ")
 	private Integer cuit;
 	private String address;
 	private String state;
 	private String country;
-	private Integer Telephone;
+	private Integer telephone;
 	private String web;
 	private String mail;
 
