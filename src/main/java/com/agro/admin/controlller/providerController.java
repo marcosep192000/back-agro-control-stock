f07a3301d90b5b5a0f9441c96eb33175c68973f0
marcos <marcosep192000@hotmail.com>
2023-04-24 22:07:49 -0300

    modify providerController create addProvider

--- src/main/java/com/agro/admin/controlller/providerController.java
+++ src/main/java/com/agro/admin/controlller/providerController.java
@@ -1,25 +1,36 @@
 package com.agro.admin.controlller;
 
+import com.agro.admin.models.entity.Provider;
 import com.agro.admin.models.request.ProductRequest;
 import com.agro.admin.models.request.ProviderRequest;
 import com.agro.admin.models.response.ProductResponse;
+import com.agro.admin.models.response.ProviderResponse;
 import com.agro.admin.service.IProviderService;
+import org.apache.coyote.Response;
+import org.hibernate.validator.constraints.CreditCardNumber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/provider")
 public class providerController {
 
 	 @Autowired
 	IProviderService providerService ;
 
-//	 @PostMapping("/create-product/{idCategory}")
-//	public ResponseEntity<Void> saveProduct(@RequestBody @Validated ProductRequest productRequest , @PathVariable Long id) {
-//
-//		return new ResponseEntity(responseEntity.getBody(),responseEntity.getStatusCode());
-//	}
+	 @PostMapping("/create")
+	public ResponseEntity<Provider> create(@Valid  @RequestBody  ProviderRequest providerRequest ) {
+          ResponseEntity<Provider> response = providerService.create(providerRequest);
+		  return new ResponseEntity(response.getBody(),response.getStatusCode());
+	}
+
+
+
 //	@PutMapping("/update-product/{idCategory}")
 //	public ResponseEntity<Void> updateProduct(@RequestBody @Validated ProviderRequest providerRequest , @PathVariable Long id) {
 //
