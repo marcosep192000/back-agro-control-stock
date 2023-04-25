bd13ee18629467658b0153e973385da570b0733d
marcos <marcosep192000@hotmail.com>
2023-04-24 22:06:58 -0300

    modify Provider Repository

--- src/main/java/com/agro/admin/service/IProviderService.java
+++ src/main/java/com/agro/admin/service/IProviderService.java
@@ -1,6 +1,8 @@
 package com.agro.admin.service;
 
 import com.agro.admin.models.entity.Provider;
+import com.agro.admin.models.request.ProviderRequest;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +10,7 @@ import java.util.Optional;
 public interface IProviderService {
     public List<Provider> getAll() ;
     public Optional<Provider> getById(Long id) ;
-    public Provider create( Provider Provider);
+    public ResponseEntity<Provider> create(ProviderRequest providerRequest);
     public Provider update(Provider Provider);
     public void delete(Long id) ;
 }
