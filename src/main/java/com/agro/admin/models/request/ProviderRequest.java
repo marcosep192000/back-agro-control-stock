bc336bff9542f068e27f0aae615e82fe535ee641
marcos <marcosep192000@hotmail.com>
2023-04-24 22:08:05 -0300

    modify ProviderRequest

--- src/main/java/com/agro/admin/models/request/ProviderRequest.java
+++ src/main/java/com/agro/admin/models/request/ProviderRequest.java
@@ -1,22 +1,20 @@
 package com.agro.admin.models.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 public class ProviderRequest {
 	@NotBlank(message = " i can't be empty ")
 	private String nameProvider;
-	@NotBlank(message = " i can't be empty ")
 	private Integer cuit;
 	private String address;
 	private String state;
 	private String country;
-	private Integer Telephone;
+	private Integer telephone;
 	private String web;
 	private String mail;
 }
