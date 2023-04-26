af22dc132bfc978363e0833ccd82c66dd0f472d2
marcos <marcosep192000@hotmail.com>
2023-04-26 10:28:50 -0300

    modify MarcaController

--- src/main/java/com/agro/admin/controlller/MarcaController.java
+++ src/main/java/com/agro/admin/controlller/MarcaController.java
@@ -1,4 +1,24 @@
 package com.agro.admin.controlller;
 
+import com.agro.admin.models.entity.Marca;
+import com.agro.admin.models.entity.Provider;
+import com.agro.admin.models.request.MarcaRequest;
+import com.agro.admin.models.request.ProviderRequest;
+import com.agro.admin.repository.MarcaRepository;
+import com.agro.admin.service.impl.MarcaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
 public class MarcaController {
+    @Autowired
+    MarcaServiceImpl service;
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@Valid @RequestBody MarcaRequest request){
+        ResponseEntity<?> response =  service.create(request);
+        return new ResponseEntity(response.getBody(),response.getStatusCode());
+    }
 }
