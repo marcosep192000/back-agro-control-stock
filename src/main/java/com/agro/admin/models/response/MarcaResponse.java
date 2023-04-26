961ac2af6fe4d3cff76785f07f75303325de0a4f
marcos <marcosep192000@hotmail.com>
2023-04-26 10:29:42 -0300

    modify MarcaResponse

--- src/main/java/com/agro/admin/models/response/MarcaResponse.java
+++ src/main/java/com/agro/admin/models/response/MarcaResponse.java
@@ -1,4 +1,18 @@
 package com.agro.admin.models.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MarcaResponse {
+    private Long id;
+    private String nameMarca;
+    private String status ;
 }
