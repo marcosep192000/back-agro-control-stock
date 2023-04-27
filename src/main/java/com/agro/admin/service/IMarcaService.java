77336b6e898cf9452ae0ffeb2054067b1294dca3
marcos <marcosep192000@hotmail.com>
2023-04-27 10:04:58 -0300

    :construction: modify IMarcaService

--- src/main/java/com/agro/admin/service/IMarcaService.java
+++ src/main/java/com/agro/admin/service/IMarcaService.java
@@ -4,6 +4,7 @@ import com.agro.admin.models.entity.Marca;
 import com.agro.admin.models.entity.Provider;
 import com.agro.admin.models.request.MarcaRequest;
 import com.agro.admin.models.response.MarcaResponse;
+import net.bytebuddy.agent.builder.AgentBuilder;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
@@ -16,7 +17,8 @@ public interface IMarcaService {
     public Marca getById(Long id) ;
     public ResponseEntity<MarcaResponse> create(MarcaRequest marcaRequest);
 
-    public void update(Marca marca);
+    public ResponseEntity<?> update(MarcaRequest request,Long id);
 
-    public void delete(Long id) ;
+
+    public ResponseEntity<?> delete(Long id) ;
 }
