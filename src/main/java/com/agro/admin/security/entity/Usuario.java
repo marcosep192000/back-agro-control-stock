420d0a092ee11b15d48748d10b1198126a1ecc40
marcos <marcosep192000@hotmail.com>
2023-05-31 22:15:16 -0300

    :construction:refactor: User

--- src/main/java/com/agro/admin/security/entity/Usuario.java
+++ src/main/java/com/agro/admin/security/entity/Usuario.java
@@ -8,12 +8,10 @@ import java.util.Set;
 @Entity
 public class Usuario {
 
-		//Id de la tabla
 		@Id
-		//Id Auto Increment
+
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		private int idUsuario;
-		//Decorador para indicar que no puede ser null el campo
 		@NotNull
 		private String nombre;
 		@NotNull
@@ -24,15 +22,8 @@ public class Usuario {
 		private String email;
 		@NotNull
 		private String password;
-
 		@NotNull
-		//Relación many to many
-		//Un usuario puede tener MUCHOS roles y un rol puede PERTENECER a varios usuarios
-		//Tabla intermedia que tiene dos campos que va a tener idUsuario y idRol
 		@ManyToMany
-		// join columns hace referencia a la columna que hace referencia hacia esta
-		// Es decir la tabla usuario_rol va a tener un campo que se llama id_usuario
-		// inverseJoinColumns = el inverso, hace referencia a rol
 		@JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "id_usuario"),
 				inverseJoinColumns = @JoinColumn(name = "rol_id"))
 		private Set<Rol> roles = new HashSet<>();
